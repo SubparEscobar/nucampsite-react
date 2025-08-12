5bd0a8481e600aaeafeeebc0658914865c25ef93
SubparEscobar <aaron.escobar1997@gmail.com>
2025-08-11 19:09:53 -0500

    Destructuring Assignment in CampsiteCard.js

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,11 +1,15 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite; // Destructuring for cleaner code
+    //Alternatively, you can use props.campsite directly
+    //props.campsite.image and props.campsite.name can be used directly if destructuring is not preferred
+    //const { campsite } = props; (Destructuring can be used here if preferred)
     return (
         <Card>
-            <CardImg width='100%' src={props.campsite.image} alt={props.campsite.name} />
+            <CardImg width='100%' src={image} alt={name} />
             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     )
