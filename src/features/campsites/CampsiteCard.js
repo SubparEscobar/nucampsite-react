23640797d3aec04131ec2fb035e5e26be0d8ba2c
SubparEscobar <aaron.escobar1997@gmail.com>
2025-08-15 10:31:41 -0500

    CampsiteDetailPage with useParams

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,17 +1,20 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const CampsiteCard = ({ campsite }) => {
-    const { image, name } = campsite; // Destructuring for cleaner code
+    const { id, image, name } = campsite; // Destructuring for cleaner code
     //Alternatively, you can use props.campsite directly
     //props.campsite.image and props.campsite.name can be used directly if destructuring is not preferred
     //const { campsite } = props; (Destructuring can be used here if preferred)
     return (
-        <Card>
-            <CardImg width='100%' src={image} alt={name} />
-            <CardImgOverlay>
-                <CardTitle>{name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg width='100%' src={image} alt={name} />
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     )
 };
 
